5bae99474c4f13929cb1f0b0d2343ad4c18f16a7
Peter Montalvo Garcia <43070511+PeterTXS09@users.noreply.github.com>
2024-06-20 14:04:04 -0500

    Update main.js

--- static/main.js
+++ static/main.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: '/static/assets/targets/targets (2).mind',
+      imageTargetSrc: '/static/assets/targets/targets (3).mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
@@ -16,17 +16,12 @@ document.addEventListener('DOMContentLoaded', () => {
     scene.add(light);
 
     //const raccoon = await loadGLTF('/static/assets/models/musicband-raccoon/scene.gltf');
-    const raccoon = await loadGLTF('/static/assets/models/upch-dia/images (1).gltf');
-    raccoon.scene.scale.set(0.009, 0.009, 0.009);
+    const raccoon = await loadGLTF('/static/assets/models/musicband-raccoon/scene.gltf');
+    raccoon.scene.scale.set(0.1, 0.1, 0.1);
     raccoon.scene.position.set(0, -0.4, 0);
-
-    const titulo = await loadGLTF('/static/assets/models/upch-dia/upch.gltf');
-    titulo.scene.scale.set(0.01, 0.01, 0.01);
-    titulo.scene.position.set(0, -1.2, 0);
 
     const anchor = mindarThree.addAnchor(0);
     anchor.group.add(raccoon.scene);
-    anchor.group.add(titulo.scene);
 
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
